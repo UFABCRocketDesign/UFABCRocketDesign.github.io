426fbd9f3ce72615a7e34af517781275ae4df740
Matheus Alefe <matheus.orvati@hotmail.com>
2024-03-04 14:43:41 -0300

    Ajustar rodapé

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,36 +1,40 @@
 import styles from './footer.module.scss';
-import { InstagramIcon, FacebookIcon, TwitterIcon, LinkedinIcon, YoutubeIcon } from '../components/icons'
+import { InstagramIcon, TwitterIcon, LinkedinIcon } from '../components/icons'
 import Link from 'next/link';
+import Image from "next/image";
 
 export default function Footer() {
+    const linkInstagram:string = "https://www.instagram.com/ufabcrocketdesign/";
+    const linkX:string = "https://twitter.com/UFABCRD";
+    const linkLinkedIn:string = "https://www.linkedin.com/company/ufabc-rocket-design/";
+
     return (
         <footer className={styles.footer}>
             <div className={styles.socialIcons}>
-                <div className={styles.socialIndIcon}><InstagramIcon /></div>
-                <div className={styles.socialIndIcon}><FacebookIcon /></div>
-                <div className={styles.socialIndIcon}><TwitterIcon /></div>
-                <div className={styles.socialIndIcon}><LinkedinIcon /></div>
-                <div className={styles.socialIndIcon}><YoutubeIcon /></div>
+                <Link href={linkInstagram} className={styles.socialIndIcon}><InstagramIcon /></Link>
+                <Link href={linkX} className={styles.socialIndIcon}><TwitterIcon /></Link>
+                <Link href={linkLinkedIn} className={styles.socialIndIcon}><LinkedinIcon /></Link>
             </div>
             <div className={styles.row}>
                 <div className={styles.logo}>
-                    <Link href="\" passHref className={styles.textLogo}><span>UFABC Rocket Design</span></Link>
+                    <Link href="\" passHref className={styles.textLogo}><span className={styles.grupoLogo}><Image src="/images/logo_rocket_azul.png" fill alt="Logomarca UFABC Rocket Design" /> UFABC Rocket Design</span></Link>
                 </div>
                 <div className={styles.quote}>
-                    <p className={styles.quoteText}>&quot;Alguma frase bonita&quot;
+                    <p className={styles.quoteText}>&quot;Per Aspera Ad Astra&quot;
                     </p>
-                    <p className={styles.quoteAuthor}>- Buscando
+                    <p className={styles.quoteAuthor}>Pelas dificuldades, aos astros
                     </p>
                 </div>
                 <div className={styles.nav}>
-                    <Link href='team' passHref className={styles.navItem}>Quem somos</Link>
-                    <Link href='' passHref className={styles.navItem}>Pesquisa</Link>
-                    <Link href='' passHref className={styles.navItem}>Impacto</Link>
-                    <Link href='' passHref className={styles.navItem}>Atuação</Link>
+                    <Link href='team' passHref className={styles.navItem}>A entidade</Link>
+                    <Link href='' passHref className={styles.navItem}>A equipe</Link>
+                    <Link href='' passHref className={styles.navItem}>Missões</Link>
+                    <Link href='' passHref className={styles.navItem}>Notícias</Link>
+                    <Link href='' passHref className={styles.navItem}>Apoio</Link>
                 </div>
             </div>
             <div className={styles.credits}>
-                <p> Freedxm Development. All rights reserved.</p>
+                <p> UFABC Rocket Design - Departamento de TI. All rights reserved.</p>
             </div>
 
         </footer>
