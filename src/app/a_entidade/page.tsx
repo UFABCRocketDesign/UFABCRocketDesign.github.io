fdc77a93b1b02de073a4ebf8d6fde6b9bda698fa
Matheus Alefe <matheus.orvati@hotmail.com>
2024-04-10 00:47:11 -0300

    Iniciar página "A Entidade"

--- src/app/a_entidade/page.tsx
+++ src/app/a_entidade/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from 'next';
-
+import Image from "next/image";
 import styles from '../../styles/a_entidade.module.scss'
 
 export const metadata: Metadata = {
@@ -8,21 +8,25 @@ export const metadata: Metadata = {
 }
 
 export default function About() {
-    return (<>
-        <div className={styles.container}>
-            <h1 className={styles.title}>Conheça nossa Equipe</h1>
-            <p className={styles.description}>
-            “Sanity, you say? In this accursed world, are there any sane men? Everyone… they’re all the same. Equally ignorant. They have no idea what it takes to love. They don’t understand… the meaning of life and death. They don’t even know why they fight.” – Canute <br/>
-            “War always produces lots of slaves. Those defeated in battle become slaves. Like you and Arnheid. The line between warrior and slave trader is blurry. Slaves come from other places too, but I think war is the number one source. If the number of wars decreased, so would the number of slaves. But Norse men don’t think war is a bad thing. A Norse man’s worth is determined by his valor and wealth. The more enemies you kill and wealth you bring back home, the more you’re respected. So, fathers teach sons to fight, arm them, and send them off on warships. It’s what the Nords have done since ancient times without questioning it. It’s always difficult to stop doing what you think is natural.”
-            </p>
-        </div>
-
-        {/* TODO: Implementar a timeline usando git-style */}
-        <div className={styles.container}>
-            <h2 className={styles.title}>Nossa trajetória</h2>
-
-            
-        </div>
+    return (
+    <>
+        <section className={`${styles.container} ${styles.stars}`}>
+            <div className={styles.historia}>
+                <h1>A entidade</h1>
+                <h2>História</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nisi vel dolor laoreet rhoncus quis in massa. Nunc fermentum quam commodo elementum posuere. Nam eu varius magna. Aliquam pulvinar finibus semper. Integer a est lorem. Proin non aliquet eros. Sed tempor imperdiet nulla, nec scelerisque purus gravida nec.</p>
+                <p>Phasellus tempus, sapien sit amet pulvinar lacinia, orci est scelerisque lacus, a aliquet arcu ipsum non nisi. Praesent pharetra id lacus sed maximus. Nulla vehicula diam quis egestas vestibulum. Vivamus in vestibulum sem. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin eget scelerisque enim, et venenatis arcu. Aliquam erat volutpat. </p>
+            </div>
+            <div className={styles.divisao}>
+                <Image className={styles.foguete} src="/images/foguete.png" fill alt="Logomarca UFABC Rocket Design" />
+            </div>
+            <div className={styles.mvo}>
+                <h2>Missão e Valores</h2>
+                <p>Sed a ultricies turpis, vitae vehicula nibh. Etiam sollicitudin justo vel nunc aliquet ultrices. Suspendisse laoreet eu enim eu venenatis. Donec porttitor consequat lectus. </p>
+                <h2>Organização</h2>
+                <p>Nulla convallis diam ex, quis viverra ipsum laoreet at. Proin vitae ex sit amet justo dapibus sodales ac eu nisl. Cras non massa eros.</p>
+            </div>
+        </section>
     </>
     ) 
 }
