dc2913b841e8ee6ab1b072da435b78fd57cd27c4
Matheus Alefe <matheus.orvati@hotmail.com>
2024-04-13 19:49:05 -0300

    Ajustes gerais de responsividade

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,22 @@
+'use client'
 import styles from './header.module.scss';
 import Link from 'next/link';
 import Image from 'next/image';
+import React, {useState} from 'react';
 
+export default function Header(){
+    const [menuStatus, setMenuStatus] = useState<boolean>(false);
 
-export default function Header(): JSX.Element {
     return (
         <header className={styles.header}>
             <Link href="/" passHref className={styles.logo}>
-                <Image className={styles.logoImg} src="/images/logo_rocket.png" fill alt="Logo" />
+                <Image onClick={() => setMenuStatus(false)} className={styles.logoImg} src="/images/logo_rocket.png" fill alt="Logo" />
                 <div className={styles.nameDiv}>
                     <h1>UFABC</h1>
                     <h1>ROCKET DESIGN</h1>
                 </div>
             </Link>
-
+            
             <ul className={styles.nav}>
                 <li ><Link href="a_entidade" passHref className={styles.navLink}>A ENTIDADE</Link></li>
                 <li ><Link href="a_equipe" passHref className={styles.navLink}>A EQUIPE</Link></li>
@@ -22,6 +25,24 @@ export default function Header(): JSX.Element {
                 <li ><Link href="apoio" passHref className={styles.navLink}>APOIO</Link></li>
             </ul>
 
+            <div className={styles.hamburgerMenu}>
+                <Image src="/images/icone-menu.png" fill alt="menu de navegação" onClick={() => setMenuStatus(!menuStatus)} />
+            </div>
+            {menuStatus? 
+                <div className={styles.blur}>
+                    <div className={styles.menuNav}>
+                        <ul className={styles.menuNavList}>
+                            <li ><Link onClick={() => setMenuStatus(false)} href="a_entidade" passHref className={styles.navLink}>A ENTIDADE</Link></li>
+                            <li ><Link onClick={() => setMenuStatus(false)} href="a_equipe" passHref className={styles.navLink}>A EQUIPE</Link></li>
+                            <li ><Link onClick={() => setMenuStatus(false)} href="missoes" passHref className={styles.navLink}>MISSÕES</Link></li>
+                            <li ><Link onClick={() => setMenuStatus(false)} href="noticias" passHref className={styles.navLink}>NOTÍCIAS</Link></li>
+                            <li ><Link onClick={() => setMenuStatus(false)} href="apoio" passHref className={styles.navLink}>APOIO</Link></li>
+                        </ul>
+                    </div>
+                </div>
+                
+            :
+            <></>}
         </header>
     );
 }
