d487d487a827c58ff6af9979f8613dceef7dca53
Matheus Alefe <matheus.orvati@hotmail.com>
2024-05-17 13:05:58 -0300

    Remover memes e imagens inuteis

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -14,21 +14,7 @@ export default function NotFound() {
             <div className={styles.row}>
                 <div className={styles.textBox}>
                     <h1 className="mb-3">404 - Page Not Found</h1>
-                    <p className="mb-4">Sentimos muito, mas o One Piece não está aqui.</p>
                 </div>
-
-                <Image src="/images/Roger.webp" alt=""
-                    width={500}
-                    height={1500}
-                    style={{
-                        width: "100%",
-                        height: "auto",
-                        borderRadius: "2rem",
-                        flex: "1 0 0%",
-                        margin: "1rem !important",
-                        overflow: "hidden",
-                    }}
-                />
 
             </div>
         </div>
