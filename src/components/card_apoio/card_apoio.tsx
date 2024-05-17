54641b66fc5bbf435635ca96a8d28b82149f969a
Matheus Alefe <matheus.orvati@hotmail.com>
2024-05-17 17:11:17 -0300

    Adicionar links aos patrocinadores

--- src/components/card_apoio/card_apoio.tsx
+++ src/components/card_apoio/card_apoio.tsx
@@ -3,15 +3,16 @@ import Image from "next/image";
 interface CardApoioProps{
     nome: string
     logo: string
+    link: string
 }
 
-export function CardApoio({nome, logo}:CardApoioProps){
+export function CardApoio({nome, logo, link}:CardApoioProps){
     return (
-        <div className={styles.container}>
+        <a className={styles.container} href={link} target='_blank'>
             <div className={styles.centralizarImagem}>
                 <Image fill src={logo} alt={`logomarca da empresa ${nome}`} className={styles.logo}/>
             </div>
             <h3 className={styles.nome}>{nome}</h3>
-        </div>
+        </a>
     )
 }
